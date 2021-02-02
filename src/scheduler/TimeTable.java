932cf311748a6344060a3243c905c34cf469ec2b
sainikhilreddy <bennikhilreddy@gmail.com>
2021-02-02 20:28:36 +0530

    5th Commit

--- src/scheduler/TimeTable.java
+++ src/scheduler/TimeTable.java
@@ -18,40 +18,7 @@ public class TimeTable {
 		// creating as many slots as the no of blocks in overall timetable
 		slot = new Slot[hours * days * nostgrp];
 
-		// looping for every student group
-		for (int i = 0; i < nostgrp; i++) {
-
-			subjectno = 0;
-			// for every slot in a week for a student group
-			for (int j = 0; j < hours * days; j++) {
-
-				StudentGroup sg = inputdata.studentgroup[i];
-
-				// if all subjects have been assigned required hours we give
-				// free periods
-				if (subjectno >= sg.nosubject) {
-					slot[k++] = null;
-				}
-
-				// if not we create new slots
-				else {
-
-					slot[k++] = new Slot(sg, sg.teacherid[subjectno], sg.subject[subjectno]);
-
-					// suppose java has to be taught for 5 hours then we make 5
-					// slots for java, we keep track through hourcount
-					if (hourcount < sg.hours[subjectno]) {
-						hourcount++;
-					} else {
-						hourcount = 1;
-						subjectno++;
-					}
-
-				}
-
-			} // end inner for
-
-		} // end outer for
+		
 
 	}// end constructor
 
